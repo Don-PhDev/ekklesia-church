0b0ec0f0e1aaa32b7cf29f1dbae10757bfce1c6b
Don Forrest <forrest@philippinedev.com>
2022-04-02 14:07:55 +0800

    Generate scaffold for Post

--- db/migrate/20220402060018_devise_create_users.rb
+++ db/migrate/20220402060018_devise_create_users.rb
@@ -4,9 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.boolean :admin,              default: false
+      t.string  :name
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -32,8 +33,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
-      t.boolean :admin, default: false
 
       t.timestamps null: false
     end
