7ae808b51e9ff8ff1dad39f9fb7cdfd700fc511b
Don Forrest <forrest@philippinedev.com>
2022-04-04 00:18:20 +0800

    Refactor, remove duplicated codes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,17 @@
 class Post < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
   belongs_to :user
 
   validates :title, :body, presence: true, allow_blank: false
 
   scope :by_recently_updated, -> { order(updated_at: :desc) }
+
+  def posted_time_passed
+    if self.updated_at - self.created_at > 1
+      "Edited " + time_ago_in_words(self.updated_at) + " ago"
+    else
+      "Posted " + time_ago_in_words(self.created_at) + " ago"
+    end
+  end
 end
