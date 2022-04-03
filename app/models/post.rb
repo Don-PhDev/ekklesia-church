a00d062d306cb5e3b2310f4b72bbe4b0adf1b55c
Don Forrest <forrest@philippinedev.com>
2022-04-04 00:15:36 +0800

    Posts order by most recent update

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, :body, presence: true, allow_blank: false
+
+  scope :by_recently_updated, -> { order(updated_at: :desc) }
 end
