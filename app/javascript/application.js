a24946df88fbc6952a0e26d52bca5f9a0dca6fa8
Don Forrest <forrest@philippinedev.com>
2022-04-13 00:15:54 +0800

    Convert javascript links to internal files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,8 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+import "bootstrap.bundle.min.js" //https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+import "purecounter_vanilla.js" // https://cdn.jsdelivr.net/npm/@srexi/purecounterjs/dist/purecounter_vanilla.js">
+import "glightbox.min.js" // https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js">
+import "swiper-bundle.min.js" // https://unpkg.com/swiper@8/swiper-bundle.min.js">
+import "isotope.pkgd.min.js" // https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js">
