65a6ef7a44867be46be04831e004a45fc61da90c
Don Forrest <forrest@philippinedev.com>
2022-04-19 23:48:53 +0800

    Add active_link_to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_link_to(path)
+    content_tag(:li, class: "#{'active' if current_page?(path)}") do
+      link_to path, class: "#{'active' if current_page?(path)} nav-link" do
+        yield
+      end
+    end
+  end
 end
